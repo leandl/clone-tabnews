9c88f8a2fb67955078e0d3cba4be4ef4322f39e6
leandro <50909151+leandl@users.noreply.github.com>
2023-10-31 11:09:38 +0000

    add  and  files

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,8 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import database from "../../../../infra/database";
 
 export default async function status(
-  request: NextApiRequest,
+  _request: NextApiRequest,
   response: NextApiResponse
 ) {
+  const result = await database.query("SELECT 1 + 1 as SUM");
+  console.log(result.rows);
   return response.status(200).json({ chave: "valor" });
 }
