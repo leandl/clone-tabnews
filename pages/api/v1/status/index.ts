ac480e70a60e40b7852285421af2b3f6fd52d887
leandl <leandrocrispim891@gmail.com>
2024-02-24 13:06:51 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -2,10 +2,36 @@ import { NextApiRequest, NextApiResponse } from "next";
 import database from "@/infra/database";
 
 export default async function status(
-  _request: NextApiRequest,
+  request: NextApiRequest,
   response: NextApiResponse
 ) {
-  const result = await database.query("SELECT 1 + 1 as SUM");
-  console.log(result.rows);
-  return response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;"
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int as opened_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].opened_connections;
+
+  return response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
