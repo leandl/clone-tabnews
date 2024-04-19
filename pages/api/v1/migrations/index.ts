a81b7b0989e8da8ec73415881313b5ccd579985f
leandl <leandrocrispim891@gmail.com>
2024-04-19 20:19:17 -0300

    bug fix in the migration route

--- pages/api/v1/migrations/index.ts
+++ pages/api/v1/migrations/index.ts
@@ -3,43 +3,56 @@ import migrationRunner, { RunnerOption } from "node-pg-migrate";
 
 import { NextApiRequest, NextApiResponse } from "next";
 import database from "@/infra/database";
+import { Client as DBCLient } from "pg";
 
-export default async function migrations(
-  request: NextApiRequest,
-  response: NextApiResponse
-) {
-  if (request.method !== "GET" && request.method !== "POST") {
-    return response.status(405).json({});
-  }
-
-  const dbClient = await database.getNewClient();
-  const defaultMigrationOptions: RunnerOption = {
+function getMigrationOptions(
+  dbClient: DBCLient,
+  inLiveRun: boolean
+): RunnerOption {
+  return {
     dbClient,
-    dryRun: true,
+    dryRun: !inLiveRun,
     dir: join("infra", "migrations"),
     direction: "up",
     migrationsTable: "pgmigrations",
     verbose: true,
   };
+}
 
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
+export default async function migrations(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method!)) {
+    return response.status(405).json({});
   }
 
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
 
-    await dbClient.end();
+    const inLiveRun = request.method === "POST";
+    const migrationOptions: RunnerOption = getMigrationOptions(
+      dbClient,
+      inLiveRun
+    );
 
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(migrationOptions);
+      return response.status(200).json(pendingMigrations);
+    }
+
+    const migratedMigrations = await migrationRunner(migrationOptions);
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
     }
 
     return response.status(200).json(migratedMigrations);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient?.end();
   }
 }
---
+++ tests/integration/api/v1/migrations/other-methods.test.ts
@@ -0,0 +1,24 @@
+const allMethodForTest = ["HEAD", "PUT", "DELETE", "OPTIONS", "PATCH"] as const;
+type Method = (typeof allMethodForTest)[number];
+
+async function getMigrationsResponse(method: Method) {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method,
+  });
+  return response;
+}
+
+async function getDataBaseStatus() {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  return await response.json();
+}
+
+test("OTHER HTTP METHODS to /api/v1/migrations should not let opened connections in database", async () => {
+  for (let method of allMethodForTest) {
+    const migrationsResponse = await getMigrationsResponse(method);
+    expect(migrationsResponse.status).toBe(405);
+
+    const status = await getDataBaseStatus();
+    expect(status.dependencies.database.opened_connections).toEqual(1);
+  }
+});
