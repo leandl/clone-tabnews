35deaf953643e63bbf10c501d7164b54e192625a
leandl <leandrocrispim891@gmail.com>
2024-07-06 13:04:15 -0300

    fix "eslint" linting

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,4 +1,4 @@
-const { exec } = require("node:child_process");
+import { exec } from "node:child_process";
 
 function checkPostgres() {
   exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
