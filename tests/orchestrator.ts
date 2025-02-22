ede07c9463a221ee73a22e9e640e4d38ba7fd830
leandl <leandrocrispim891@gmail.com>
2025-02-22 11:06:47 -0300

    feat: add 'runPendingMigrations' method to 'orchestrator'

--- tests/orchestrator.ts
+++ tests/orchestrator.ts
@@ -1,3 +1,4 @@
+import migrator from "@/models/migrator";
 import retry from "async-retry";
 import database from "infra/database";
 
@@ -24,9 +25,14 @@ async function clearDatabase() {
   await database.query("drop schema public cascade; create schema public;");
 }
 
+async function runPendingMigrations() {
+  await migrator.runPendingMigrations();
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
+  runPendingMigrations,
 };
 
 export default orchestrator;
