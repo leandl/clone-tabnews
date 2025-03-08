04c997c351bb03af6d3ed8c376a81ab30360b8ed
leandl <leandrocrispim891@gmail.com>
2025-03-08 09:19:40 -0300

    feat: create 'user' model and '/api/v1/users' endpoint

---
+++ models/user.ts
@@ -0,0 +1,84 @@
+import database from "@/infra/database";
+import { ValidationError } from "@/infra/errors";
+
+type UserCreateDTO = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+async function create(userInputValues: UserCreateDTO) {
+  await validateUniqueEmail(userInputValues.email);
+  await validateUniqueUsername(userInputValues.username);
+
+  const newUser = await runInsertQuery(userInputValues);
+  return newUser;
+
+  async function validateUniqueEmail(email: string) {
+    const results = await database.query({
+      text: `
+      SELECT
+        email
+      FROM
+        users
+      WHERE
+        LOWER(email) = LOWER($1)
+      ;`,
+      values: [email],
+    });
+
+    if (results.rowCount > 0) {
+      throw new ValidationError({
+        message: "O email informado já está sendo utilizado.",
+        action: "Utilize outro email para realizar o cadastro.",
+      });
+    }
+  }
+
+  async function validateUniqueUsername(username: string) {
+    const results = await database.query({
+      text: `
+      SELECT
+        username
+      FROM
+        users
+      WHERE
+        LOWER(username) = LOWER($1)
+      ;`,
+      values: [username],
+    });
+
+    if (results.rowCount > 0) {
+      throw new ValidationError({
+        message: "O username informado já está sendo utilizado.",
+        action: "Utilize outro username para realizar o cadastro.",
+      });
+    }
+  }
+
+  async function runInsertQuery(userInputValues: UserCreateDTO) {
+    const results = await database.query({
+      text: `
+      INSERT INTO 
+        users (username, email, password) 
+      VALUES 
+        ($1, $2, $3)
+      RETURNING
+        *
+      ;`,
+      values: [
+        userInputValues.username,
+        userInputValues.email,
+        userInputValues.password,
+      ],
+    });
+
+    return results.rows[0];
+  }
+}
+
+const user = {
+  create,
+};
+
+export default user;
